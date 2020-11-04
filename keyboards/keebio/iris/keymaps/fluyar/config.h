b4935c5332076f0bae0f2418a5ad232fc2a76f02
Florian Schmitz <florian.schmitz@gmail.com>
2020-11-04 13:13:40 +0100

    Last german commit

--- keyboards/keebio/iris/keymaps/fluyar/config.h
+++ keyboards/keebio/iris/keymaps/fluyar/config.h
@@ -18,9 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #define TAPPING_TERM 300
-#define RGBLIGHT_ANIMATIONS
 #define COMBO_COUNT 4
 #define COMBO_TERM 500
 #define RGBLIGHT_SPLIT
-#define LEADER_TIMEOUT 300
-#define LEADER_PER_KEY_TIMING
+#define RGBLIGHT_LAYERS
